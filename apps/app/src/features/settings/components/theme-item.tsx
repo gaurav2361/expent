119e03d3121dfec3ad2d62d5042278be03ba2179
gaurav2361 <gaurav23617@gmail.com>
2026-04-04 09:30:08 +0530

    feat(app): obytes React Native App Kickstart added and lang removed

---
+++ apps/app/src/features/settings/components/theme-item.tsx
@@ -0,0 +1,51 @@
+import type { OptionType } from '@/components/ui';
+
+import type { ColorSchemeType } from '@/lib/hooks/use-selected-theme';
+import * as React from 'react';
+import { Options, useModal } from '@/components/ui';
+import { useSelectedTheme } from '@/lib/hooks/use-selected-theme';
+
+import { SettingsItem } from './settings-item';
+
+export function ThemeItem() {
+  const { selectedTheme, setSelectedTheme } = useSelectedTheme();
+  const modal = useModal();
+
+  const onSelect = React.useCallback(
+    (option: OptionType) => {
+      setSelectedTheme(option.value as ColorSchemeType);
+      modal.dismiss();
+    },
+    [setSelectedTheme, modal],
+  );
+
+  const themes = React.useMemo(
+    () => [
+      { label: `Dark 🌙`, value: 'dark' },
+      { label: `Light 🌞`, value: 'light' },
+      { label: `System ⚙️`, value: 'system' },
+    ],
+    [],
+  );
+
+  const theme = React.useMemo(
+    () => themes.find(t => t.value === selectedTheme),
+    [selectedTheme, themes],
+  );
+
+  return (
+    <>
+      <SettingsItem
+        text="Theme"
+        value={theme?.label}
+        onPress={modal.present}
+      />
+      <Options
+        ref={modal.ref}
+        options={themes}
+        onSelect={onSelect}
+        value={theme?.value}
+      />
+    </>
+  );
+}
